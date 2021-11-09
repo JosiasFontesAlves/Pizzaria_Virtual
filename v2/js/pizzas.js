2c187f38232fa5dbf5fd34afc3e0f83ce71a49b5
Josias <jkodja@gmail.com>
2021-11-09 17:41:47 -0300

    .

---
+++ v2/js/pizzas.js
@@ -0,0 +1,5 @@
+export default {
+    Tradicionais: ['À moda', 'Calabresa', 'Frango', 'Portuguesa'],
+    Doces: ['Banana', 'Chocolate', 'Morango', 'Romeu e Julieta'],
+    Especiais: ['Bacon', 'Cachorro-quente', 'Nutella', 'Sorvete']
+};
