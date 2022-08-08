6fb97f66d4a8ac45c31fe55e34d579b62454677b
Josias Fontes Alves <68723380+JosiasFontesAlves@users.noreply.github.com>
2022-08-08 07:15:34 -0300

    .

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ import Footer from './components/Footer.js';
 
 templatr(Header, Root, Footer);
 
-location.hash = '#home';
+location.hash = '#pedido';
 
 const carrinho = {};
 
@@ -15,22 +15,22 @@ const setCarrinho = () =>
 setCarrinho();
 
 selekFn('#root', 'click', ev => {
-    if (ev.target.localName === 'button') {
-        const [pizza, spanPizza] = [
-            ev.composedPath()[2],
-            ev.composedPath()[1]
-        ].map(({ children }, i) => children[i]);
+    if (ev.target.localName !== 'button') return;
 
-        const fnBtn = {
-            btn_menos: num => num > 0 ? num - 1 : 0,
-            btn_mais: num => num + 1
-        }
+    const [pizza, spanPizza] = [
+        ev.composedPath()[2],
+        ev.composedPath()[1]
+    ].map(({ children }, i) => children[i]);
 
-        const qtdePizzas = () =>
-            carrinho[pizza.textContent] = fnBtn[getSubstring(ev.target.className, /btn_m\w+/)](Number(spanPizza.textContent));
-
-        spanPizza.textContent = qtdePizzas();
-
-        setCarrinho();
+    const fnBtn = {
+        btn_menos: num => num > 0 ? num - 1 : 0,
+        btn_mais: num => num + 1
     }
+
+    const qtdePizzas = () =>
+        carrinho[pizza.textContent] = fnBtn[getSubstring(ev.target.className, /btn_m\w+/)](Number(spanPizza.textContent));
+
+    spanPizza.textContent = qtdePizzas();
+
+    setCarrinho();
 });
--- src/js/setCarrinho.js
+++ src/js/setCarrinho.js
@@ -4,25 +4,25 @@ import Item from './components/Item.js';
 const valorTotal = [];
 
 export default hash => {
-    if (hash === '#carrinho') {
-        const Pizzas = mapEntries(JSON.parse(localStorage.getItem('pizzaria')), ([pizza, qtde]) => {
-            const [sabor, valor] = pizza.split('R$');
+    if (hash !== '#carrinho') return;
 
-            valorTotal.push(Number(valor * qtde));
+    const Pizzas = mapEntries(JSON.parse(localStorage.getItem('pizzaria')), ([pizza, qtde]) => {
+        const [sabor, valor] = pizza.split('R$');
 
-            return qtde > 0 ? Item(qtde, sabor, valor) : '';
-        });
+        valorTotal.push(Number(valor * qtde));
 
-        insertChilds(hash, [
-            ...Pizzas,
-            render({
-                div: {
-                    className: 'flex item padd5'
-                }
-            }, [
-                render({ b: { className: 'item_sabor' } }, 'Valor total: '),
-                Span(`R$${valorTotal.reduce((a, b) => a + b)}`, { className: 'item_valor' })
-            ])
-        ]);
-    }
+        return qtde > 0 ? Item(qtde, sabor, valor) : '';
+    });
+
+    insertChilds(hash, [
+        ...Pizzas,
+        render({
+            div: {
+                className: 'flex item padd5'
+            }
+        }, [
+            render({ b: { className: 'item_sabor' } }, 'Valor total: '),
+            Span(`R$${valorTotal.reduce((a, b) => a + b)}`, { className: 'item_valor' })
+        ])
+    ]);
 };
